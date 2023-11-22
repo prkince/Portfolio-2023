533ad49c69fa5bac8ae9d34990d8bae72d5c1613
Pierrick Delage <pierrick.delage@gmail.com>
2023-11-22 03:21:05 +0100

    simple portfolio 2023

---
+++ js/scripts.js
@@ -0,0 +1,59 @@
+$(function () {
+
+    // init feather icons
+    feather.replace();
+
+    //page scroll
+    $('a.page-scroll').bind('click', function (event) {
+        let $anchor = $(this);
+        $('html, body').stop().animate({
+            scrollTop: $($anchor.attr('href')).offset().top - 20
+        }, 1000);
+        event.preventDefault();
+    });
+
+    // slick slider
+    $('.slick-about').slick({
+        slidesToShow: 1,
+        slidesToScroll: 1,
+        autoplay: true,
+        autoplaySpeed: 3000,
+        dots: true,
+        arrows: false
+    });
+
+    //toggle scroll menu
+    let scrollTop = 0;
+    $(window).scroll(function () {
+        let scroll = $(window).scrollTop();
+        //adjust menu background
+        if (scroll > 80) {
+            if (scroll > scrollTop) {
+                $('.smart-scroll').addClass('scrolling').removeClass('up');
+            } else {
+                $('.smart-scroll').addClass('up');
+            }
+        } else {
+            // remove if scroll = scrollTop
+            $('.smart-scroll').removeClass('scrolling').removeClass('up');
+        }
+
+        scrollTop = scroll;
+
+        // adjust scroll to top
+        if (scroll >= 600) {
+            $('.scroll-top').addClass('active');
+        } else {
+            $('.scroll-top').removeClass('active');
+        }
+        return false;
+    });
+
+    // scroll top
+    $('.scroll-top').click(function () {
+        $('html, body').stop().animate({
+            scrollTop: 0
+        }, 1000);
+    });
+
+});
